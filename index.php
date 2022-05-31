e402011dc5fb53e3b60c6a7994e8841294a35f5f
drawitnow <sasha.romanuyk@gmail.com>
2022-05-31 16:01:26 +0200

    fix uri hendling

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 
 $uri = $_SERVER[REQUEST_URI];
 
-echo $uri;
-
-if ($uri === 'en') {
+if ($uri === '/en') {
     echo file_get_contents("enPage.html");	
 } else {
 	echo file_get_contents("uaPage.html");
