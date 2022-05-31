e8edc322a6a76bd5dfcfc07ca08378bb7c471ba4
drawitnow <sasha.romanuyk@gmail.com>
2022-05-31 15:58:44 +0200

    add htaccess

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $uri = $_SERVER[REQUEST_URI];
 echo $uri;
 
 if ($uri === 'en') {
-    echo file_get_contents("en.html");	
+    echo file_get_contents("enPage.html");	
 } else {
-	echo file_get_contents("en.html");
+	echo file_get_contents("uaPage.html");
 }
