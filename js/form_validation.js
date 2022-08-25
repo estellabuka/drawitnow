149f83e9d05fb749055cb06042b7cf9193127349
Dmytro Iliushchenko <d.v.ilyushchenko@gmail.com>
2022-08-25 15:09:16 +0300

    Fix tooltips

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,108 +1,28 @@
 "use strict"
 
-function sendSuccess(form) {
+function sendSuccess() {
 	const tooltip = document.getElementById('tooltip');
 	tooltip.innerHTML = '<p>Повідомлення відправлено</p>';
 	tooltip.classList.add('shown');
 }
-function sendError(form) {
+function sendError() {
 	const tooltip = document.getElementById('tooltip');
 	tooltip.innerHTML = '<p>Не вдалося відправити повідомлення</p>';
 	tooltip.classList.add('shown');	
 }
-function formValidError(form) {
+function formValidError() {
 	const tooltip = document.getElementById('tooltip');
 	tooltip.innerHTML = '<p>Будь ласка, перевірте коректність даних!</p>';
 	tooltip.classList.add('shown');	
 }
 
-function tooltipReset(form) {
+function tooltipReset() {
 	const tooltip = document.getElementById('tooltip');
 	tooltip.innerHTML = '';
 	tooltip.classList.remove('shown');
+	console.log("done");
 }
 
-//Short-form
-document.addEventListener('DOMContentLoaded', function () {
-	const shortForm = document.getElementById('shortForm');
-	shortForm.addEventListener('submit', formSend);
-
-	async function formSend(e) {
-		e.preventDefault();
-
-		let error = formValidate(shortForm);
-
-        let formData = new FormData(shortForm);
-        formData.append('image', formImage.files[0]);
-
-		if (error === 0) {
-            shortForm.classList.add('_sending');
-            let response = await fetch('sendmail.php', {
-                method: 'POST',
-                body: formData
-            });
-            if (response.ok) {
-                let result = await response.json();
-                sendSuccess(form);
-                formPreview.innerHTML = '';
-                shortForm.reset();
-                shortForm.classList.remove('_sending');
-				setTimeout(tooltipReset(form), 3000);
-            } else {
-                sendError(form);
-                shortForm.classList.remove('_sending');
-				setTimeout(tooltipReset(form), 3000);
-            }
-		} else {
-			formValidError(form);
-			setTimeout(tooltipReset(form), 3000);
-		}
-	}
-
-	function formValidate(form) {
-		let error = 0;
-		let formReq = document.querySelectorAll('._req');
-
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);
-
-			if (input.classList.contains('_email')) {
-				if (emailTest(input)) {
-					formAddError(input);
-					error++;
-				}
-			} else if (input.classList.contains('_tg')) {
-				if (tgTest(input)) {
-					formAddError(input);
-					error++;
-				}
-			} else {
-				if (input.value === '') {
-				formAddError(input);
-				error++;
-				}
-			}
-		}
-		return error;
-	}
-	function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
-	}
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
-	}
-	//email test
-	function emailTest(input) {
-		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-	}
-	//Telegram test
-	function tgTest(input) {
-		return !/^@[\w-]/.test(input.value);
-	}
-});
 
 //Pay-form
 document.addEventListener('DOMContentLoaded', function () {
@@ -125,19 +45,19 @@ document.addEventListener('DOMContentLoaded', function () {
             });
             if (response.ok) {
                 let result = await response.json();
-                sendSuccess(form);
+                sendSuccess();
                 formPreview.innerHTML = '';
                 payForm.reset();
                 payForm.classList.remove('_sending');
-				setTimeout(tooltipReset(form), 3000);
+				setTimeout(tooltipReset, 3000);
             } else {
-                sendError(form);
+                sendError();
                 payForm.classList.remove('_sending');
-				setTimeout(tooltipReset(form), 3000);
+				setTimeout(tooltipReset, 3000);
             }
 		} else {
-			formValidError(form);
-			setTimeout(tooltipReset(form), 3000);
+			formValidError();
+			setTimeout(tooltipReset, 3000);
 		}
 	}
 
